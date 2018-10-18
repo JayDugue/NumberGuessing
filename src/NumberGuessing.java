b8b2bb3f68cfcf5cc432c1b08e838ee3701124d0
Jayden Dugue <jayden.dugue.2021@student.lhschools.org>
2018-10-18 08:31:53 -0400

    more changes were made

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,3 +1,5 @@
+import javafx.scene.paint.Stop;
+
 import java.util.Scanner;
 
 public class NumberGuessing {
@@ -7,33 +9,47 @@ public class NumberGuessing {
         int pickedNumber;
         Scanner keyboard;
        int guessNumber;
+       int playAgain = 1;
+        int maxTries = 10;
 
 
         //initialization
-        pickedNumber = (int)(Math.random()*100);
+        do {
+            pickedNumber = (int) (Math.random() * 100);
 
-        System.out.println(pickedNumber);
-        keyboard = new Scanner(System.in);
-        System.out.println("Pick a Number 0-100.");
-        guessNumber = keyboard.nextInt();
-        int maxTries = 10;
-        for (int tries = 0; tries<=maxTries; tries++) {
-            if (pickedNumber > guessNumber) {
-                System.out.println("too low");
+            System.out.println(pickedNumber);
+            keyboard = new Scanner(System.in);
+            System.out.println("Pick a Number 0-100.");
+            System.out.println("You only have 10 tries");
+            guessNumber = keyboard.nextInt();
+            for (int tries = 0; tries <=maxTries; tries++) {
+
+                if ((pickedNumber != guessNumber)) {
+                    if (pickedNumber > guessNumber) {
+                        System.out.println("too low");
+                    } else if (pickedNumber < guessNumber) {
+                        System.out.println("too high");
+                    }
+                    System.out.println("guess again");
+                    System.out.println("Pick a Number 0-100");
+                    guessNumber = keyboard.nextInt();
+
+                }
+                else {
+                    System.out.println("You won");
+                    System.out.println("Do you want to play again, 1 for yes, 2 for no?");
+                    playAgain = keyboard.nextInt();
+                    break;
+                }
             }
-            else if(pickedNumber < guessNumber){
-                System.out.println("too high");
-            }
-            if ((pickedNumber != guessNumber)){
-                System.out.println("guess again");
-                guessNumber = keyboard.nextInt();
-            }
-            if(guessNumber == pickedNumber){
-                System.out.println("You won");
-                break;
+
+            if(guessNumber != pickedNumber ){
+                System.out.println("Out of tries, You Lose!");
+                System.out.println("Do you want to play again, 1 for yes, 2 for no?");
+                playAgain = keyboard.nextInt();
 
             }
-        }
+        }while (playAgain == 1);
 
 
 
